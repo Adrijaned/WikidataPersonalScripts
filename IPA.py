27b9c140495b3874a9d66c2cf7c85065ba796edc
Adrijaned <adrijaned@gmail.com>
2020-01-13 22:59:38 +0100

    Way faster and better pronunciation.py

--- IPA.py
+++ IPA.py
@@ -52,41 +52,44 @@ def pronun_to_IPA(word: str):
         result += mapping[letter]
     return result
 
+def main():
+    session = mwapi.Session(
+        host='https://www.wikidata.org',
+        auth=requests_oauthlib.OAuth1(
+            client_key=credsdsds[0],
+            client_secret=credsdsds[1],
+            resource_owner_key=credsdsds[2],
+            resource_owner_secret=credsdsds[3],
+        ),
+        user_agent="Adrijaned_custom_script"
+    )
 
-session = mwapi.Session(
-    host='https://www.wikidata.org',
-    auth=requests_oauthlib.OAuth1(
-        client_key=credsdsds[0],
-        client_secret=credsdsds[1],
-        resource_owner_key=credsdsds[2],
-        resource_owner_secret=credsdsds[3],
-    ),
-    user_agent="Adrijaned_custom_script"
-)
+    lexemeID = input("Enter ID of the lexeme (ensure all forms have single P7243-pronunciation): ")  # L204262
 
-lexemeID = input("Enter ID of the lexeme (ensure all forms have single P7243-pronunciation): ")  # L204262
+    token = session.get(action='query', meta='tokens')['query']['tokens']['csrftoken']
+    claims = [x['claims']['P7243'][0]
+              for x in
+              session.get(action="wbgetentities", ids=lexemeID)["entities"][lexemeID]["forms"]
+              if
+              'qualifiers' not in x['claims']['P7243'][0].keys()]
 
-token = session.get(action='query', meta='tokens')['query']['tokens']['csrftoken']
-claims = [x['claims']['P7243'][0]
-          for x in
-          session.get(action="wbgetentities", ids=lexemeID)["entities"][lexemeID]["forms"]
-          if
-          'qualifiers' not in x['claims']['P7243'][0].keys()]
+    claim_texts = list(dict.fromkeys([x["mainsnak"]['datavalue']['value']['text'] for x in claims]))
 
-claim_texts = list(dict.fromkeys([x["mainsnak"]['datavalue']['value']['text'] for x in claims]))
+    for word in claim_texts:
+        ipa = pronun_to_IPA(word)
+        uncertain = False
+        for x in "mncřrlm":
+            if x in word:
+                uncertain = True
+        while input(f'Is [{word}] transcribed as /{ipa}/? {"Doubtful." if uncertain else ""} (Y/N) ') != "Y":
+            ipa = input("What is the correct form then? : ")
 
-for word in claim_texts:
-    ipa = pronun_to_IPA(word)
-    uncertain = False
-    for x in "mncřrlm":
-        if x in word:
-            uncertain = True
-    while input(f'Is [{word}] transcribed as /{ipa}/? {"Doubtful." if uncertain else ""} (Y/N) ') != "Y":
-        ipa = input("What is the correct form then? : ")
+        for claim in claims:
+            if word == claim["mainsnak"]['datavalue']['value']['text']:
+                millis = int(round(time.time() * 1000))
+                session.post(action='wbsetqualifier', claim=claim['id'], property="P898", value=f'"{ipa}"',
+                             snaktype='value', token=token)
+                print(f"Submitted one, took {int(round(time.time() * 1000)) - millis} milliseconds.")
 
-    for claim in claims:
-        if word == claim["mainsnak"]['datavalue']['value']['text']:
-            millis = int(round(time.time() * 1000))
-            session.post(action='wbsetqualifier', claim=claim['id'], property="P898", value=f'"{ipa}"',
-                         snaktype='value', token=token)
-            print(f"Submitted one, took {int(round(time.time() * 1000)) - millis} milliseconds.")
+if __name__ == '__main__':
+    main()
--- pronunciation.py
+++ pronunciation.py
@@ -1,6 +1,7 @@
 import mwapi
 import requests_oauthlib
 from credentials import credsdsds
+from IPA import pronun_to_IPA
 
 session = mwapi.Session(
     host='https://www.wikidata.org',
@@ -23,12 +24,30 @@ forms_new = [x for x in forms if x["claims"] == [] or "P7243" not in x["claims"]
 forms_texts = [x["representations"]["cs"]["value"] for x in forms_new]
 forms_texts = list(dict.fromkeys(forms_texts))
 
+whatever = []
 for word in forms_texts:
     approved = False
     while not approved:
         pronunciation = input(f'Input pronunciation for {word}: ')
-        approved = input(f"Is \"{pronunciation}\" correct? (Y/N) ") == "Y"
+        approved = input(f"Is \"{pronunciation}\" correct? (Y/N) ").upper() == "Y"
+    ipa = pronun_to_IPA(pronunciation)
+    uncertain = False
+    for x in "mncřrlm":
+        if x in pronunciation:
+            uncertain = True
+    while input(f'Is [{pronunciation}] transcribed as /{ipa}/? {"Doubtful." if uncertain else ""} (Y/N) ').upper() != "Y":
+        ipa = input("What is the correct form then? : ")
     for form in forms_new:
         if word == form["representations"]["cs"]["value"]:
-            session.post(action="wbcreateclaim", entity=form['id'], snaktype='value', property="P7243",
-                         value='{"text":"' + pronunciation + '","language":"cs"}', token=token)
+            whatever.append(f'{{"id":"{form["id"]}","claims":[{{"mainsnak": {{"snaktype": "value", "property": "P7243",'
+                            f' "datavalue": {{"value": {{"language": "cs", "text": "{pronunciation}"}}, "type": '
+                            '"monolingualtext"}}, "type": "statement", "rank": "normal", "qualifiers":['
+                            '{"property":"P898","snaktype":"value","datavalue":{"type":"string","value":"'
+                            f'{ipa}'
+                            '"}},{"property":"P443","snaktype":"value","datavalue":{"type":"string","value":"'
+                            f'Cs-{word}.ogg'
+                            '"}}]}]}')
+            # session.post(action="wbcreateclaim", entity=form['id'], snaktype='value', property="P7243",
+            #              value='{"text":"' + pronunciation + '","language":"cs"}', token=token)
+session.post(action='wbeditentity', id=lexemeID, summary="Added pronunciation data", token=token,
+             data='{"forms":[' + ','.join(whatever) + ']}')
